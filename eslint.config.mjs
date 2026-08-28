e70bdee783a50ee7f308e71935377560e65f7329
piyush <piyushpatil8104@gmail.com>
2026-08-28 22:59:32 +0530

    errorsolve

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  { ignores: ["server/**", ".next/**"] },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
